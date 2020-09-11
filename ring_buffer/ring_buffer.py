0bb8ecbd3ab816ffa27b8e82a40d2cd0c2c73af7
emilme <emilme@sir.mle>
2020-09-11 09:44:22 -0700

    MVP

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.value = []
+        self.oldest_node = 0
 
     def append(self, item):
-        pass
+        if len(self.value) >= self.capacity:
+            if self.oldest_node == self.capacity:
+                self.oldest_node = 0
+            self.value.pop(self.oldest_node)
+            self.value.insert(self.oldest_node, item)
+            self.oldest_node +=1
+        else:
+            self.value.append(item)
 
     def get(self):
-        pass
+        return self.value
